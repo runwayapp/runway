a594ef492642fc6bf71f939a628e359e8226ed60
grantbirki <grantbirki@github.com>
2023-04-03 18:55:08 +0100

    update puma config

--- config/puma.rb
+++ config/puma.rb
@@ -3,15 +3,9 @@
 
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers a minimum and maximum.
-# Any libraries that use thread pools should be configured to match
-# the maximum value specified for Puma. Default is set to 5 threads for minimum
-# and maximum, this matches the default thread size of Active Record.
-#
-threads_count = ENV.fetch('PUMA_THREADS', 5).to_i
-threads threads_count, threads_count
+threads 0, 16
 
 # Specifies the `port` that Puma will listen on to receive requests, default is 3000.
-#
 port ENV.fetch('PORT', 3000)
 
 # Specifies the number of `workers` to boot in clustered mode.
@@ -19,8 +13,9 @@ port ENV.fetch('PORT', 3000)
 # the concurrency of the application would be max `threads` * `workers`.
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
-#
-workers ENV.fetch('WORKERS', 1).to_i
+# https://github.com/puma/puma#clustered-mode
+workers 3
+preload_app!
 
 # Load metrics plugin
 plugin 'metrics'
