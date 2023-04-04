62659d18e15405473c22e610d43fc1fc50d9fb17
grantbirki <grantbirki@github.com>
2023-04-04 17:14:45 +0100

    reachout to the ATC for all valid commands for a repo

--- src/lib/utils/helpers.rb
+++ src/lib/utils/helpers.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 require 'dotenv/load' # Manages environment variables
+require "httparty"
 
 # Fetches the name of the bot from the environment variables
 BOT_SELF = ENV.fetch('GITHUB_APP_NAME', nil)
+
+# Fetches the ATC endpoint from the environment variables
+ATC_ENDPOINT = ENV.fetch('ATC_ENDPOINT', nil)
 
 module GitHubApp
   module Helpers
@@ -13,6 +17,27 @@ module GitHubApp
     # Note: payload['sender']['type'] will be set to "Bot" in this context as well
     def self.from_self?(payload)
       payload['sender']['login'] == BOT_SELF
+    end
+
+    # Checks if the command issued is a registered command in the ATC
+    # :param payload: The payload of the webhook
+    # :return: the matching command (String), False otherwise
+    def self.valid_command?(payload)
+      # get all commands for the repo from the ATC
+      resp = HTTParty.get("#{ATC_ENDPOINT}/#{payload['repository']['full_name']}/commands")
+
+      raise "Error fetching commands from ATC: #{resp.code}" if resp.code != 200
+
+      # convert the response to a hash
+      data = JSON.parse(resp.body, symbolize_names: true)
+
+      # loop through all returned commands and check if the comment body matches
+      data.each do |command|
+        return command[:data][:command] if payload['comment']['body'].start_with?(command[:data][:command])
+      end
+
+      # if we get here, the command is not valid
+      return false
     end
   end
 end
