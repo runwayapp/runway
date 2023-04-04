d9fd4eece72b9d58c98734ab4abe060ba1dc7ac8
grantbirki <grantbirki@github.com>
2023-04-04 10:50:28 +0100

    adding mock data to commands table

--- fixtures/docker/database/data.sql
+++ fixtures/docker/database/data.sql
@@ -12,3 +12,20 @@ INSERT INTO organizations(name, plan) VALUES
 ('runway', 'enterprise'),
 ('monalisa', 'free'),
 ('lisamona', 'team');
+
+# the commands table
+CREATE TABLE commands (
+    id VARCHAR(255) NOT NULL PRIMARY KEY,
+    organization VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    data JSON,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO commands(id, organization, name, data) VALUES
+('5ECFDB3A-C229-4982-B5B0-5CC87B8A616A', 'runway', 'deploy command', '{"name": "deploy command", "description": "Deploy the application", "command": ".deploy"}'),
+('8FF93DAA-66DC-4398-9AD7-93A480AC8AD7', 'runway', 'linter', '{"name": "linter", "description": "it lints things", "command": ".lint"}'),
+('58890287-9FF4-4FFA-B671-05AC33B9372E', 'runway', 'help', '{"name": "help", "description": "a general help command", "command": ".help"}'),
+('5A253C4D-AE3F-4B8D-AABD-F418C34F1D1F', 'monalisa', 'help', '{"name": "help", "description": "a general help command", "command": ".help"}'),
+('E497B87C-7BC7-4565-8477-54C8F9441CD0', 'lisamona', 'help', '{"name": "help", "description": "a general help command", "command": ".help"}');
