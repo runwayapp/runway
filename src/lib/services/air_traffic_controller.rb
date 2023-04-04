c1c862882c7cb6aac14b07f54886966c62874ca3
grantbirki <grantbirki@github.com>
2023-04-04 21:23:24 +0100

    working jwt auth to the atc

--- src/lib/services/air_traffic_controller.rb
+++ src/lib/services/air_traffic_controller.rb
@@ -2,6 +2,7 @@
 
 require 'dotenv/load' # Manages environment variables
 require "httparty"
+require "jwt"
 
 module GitHubApp
   module Services
@@ -9,15 +10,54 @@ module GitHubApp
       def initialize(logger: nil)
         @log = logger || Logger.new($stdout, level: ENV.fetch('LOG_LEVEL', 'INFO').upcase)
         @endpoint = ENV.fetch('ATC_ENDPOINT', nil)
+        @jwt_token = nil
+        @jwt_token_raw = nil
+        @api_key_headers = {
+          "x-api-key": ENV.fetch('GITHUB_APP_API_KEY', nil)
+        }
+        @refresh_payload = {
+          "login": ENV.fetch('GITHUB_APP_NAME', nil)
+        }.to_json
       end
 
       def refresh
-        true
+        # skip if we still have a valid JWT token
+        if !@jwt_token.nil? && @jwt_token[0]["authorized"] == true && (Time.now.to_i < @jwt_token[0]["exp"])
+          @log.debug("JWT token for the ATC is still valid, skipping refresh")
+          return
+        end
+
+        @log.debug("refreshing JWT token for the ATC")
+
+        # make an API call to get a fresh JWT token
+        resp = HTTParty.post("#{@endpoint}/auth", headers: @api_key_headers, body: @refresh_payload)
+
+        # check if the response was successful
+        if resp.code != 200
+          @log.error("Error fetching JWT token from ATC: #{resp.code}")
+          return
+        end
+
+        # parse the response as JSON
+        data = JSON.parse(resp.body, symbolize_names: true)
+
+        # save the JWT token in the instance variables
+        @jwt_token = JWT.decode(data[:token], nil, false)
+        @jwt_token_raw = data[:token]
+        @log.debug("JWT token for the ATC refreshed")
+
+        @log.debug(@jwt_token[0])
+      end
+
+      def headers
+        {
+          "Authorization": "Bearer #{@jwt_token_raw}"
+        }
       end
 
       def get(path)
         # get all commands for the repo from the ATC
-        HTTParty.get("#{@endpoint}#{path}")
+        HTTParty.get("#{@endpoint}#{path}", headers:)
       end
     end
   end
