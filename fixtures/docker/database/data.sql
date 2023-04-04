c5f11457cb388438195414d8389c08cf80bebadc
grantbirki <grantbirki@github.com>
2023-04-04 10:29:28 +0100

    working org db

--- fixtures/docker/database/data.sql
+++ fixtures/docker/database/data.sql
@@ -1,10 +1,14 @@
-# create a table with some data
-CREATE TABLE student (
-    id int,
-    name VARCHAR(255)
+# the organizations table
+# an organization can either be a GitHub organization or a GitHub user
+CREATE TABLE organizations (
+    name VARCHAR(255) NOT NULL PRIMARY KEY,
+    plan VARCHAR(255) NOT NULL,
+    members JSON,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO student(id, name) VALUES
-(1,'A'),
-(2,'B'),
-(3,'C');
+INSERT INTO organizations(name, plan) VALUES
+('runway', 'enterprise'),
+('monalisa', 'free'),
+('lisamona', 'team');
